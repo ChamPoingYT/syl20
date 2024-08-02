0a6422330088f9eb5442edb9b2e94de25f80938d
ChamPoingYT <champoing.pro@gmail.com>
2024-08-02 18:17:01 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,20 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = ""; // Mettre votre mot de passe ici
-$dbname = "lycee";
+// Remplacez ceci par l'hôte de la base de données MySQL fourni par Railway
+$servername = "mysql.railway.internal"; // Exemple : "us-cdbr-east-04.cleardb.com"
+$username = "root"; // Nom d'utilisateur
+$password = "gmwfiwDEPtcpnKLXSJjpKAvQFoOuNqbB"; // Mot de passe
+$dbname = "railway"; // Nom de la base de données
 
-// Créer une connexion
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Crée une connexion à la base de données MySQL
+$mysqli = new mysqli($servername, $username, $password, $dbname);
 
-// Vérifier la connexion
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Vérifiez la connexion
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
 }
+
+// Configurez les paramètres du charset (optionnel mais recommandé)
+$mysqli->set_charset("utf8");
+
+// Vous pouvez maintenant utiliser $mysqli pour vos opérations sur la base de données
 ?>
